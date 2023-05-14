89de73fba1b038d1d85974c1c1320de6c163656a
Aman Gupta <68396428+aman97703@users.noreply.github.com>
2023-05-14 14:42:49 +0530

    crete image

--- src/Components/Loader.js
+++ src/Components/Loader.js
@@ -3,11 +3,11 @@ import React from "react";
 const Loader = () => {
   return (
     <div className="loader-root">
-      <div class="wrapper">
-        <div class="dot dot-1" id="dot-1"></div>
-        <div class="dot dot-2" id="dot-2"></div>
-        <div class="dot dot-3" id="dot-3"></div>
-        <div class="dot dot-4" id="dot-4"></div>
+      <div className="wrapper">
+        <div className="dot dot-1" id="dot-1"></div>
+        <div className="dot dot-2" id="dot-2"></div>
+        <div className="dot dot-3" id="dot-3"></div>
+        <div className="dot dot-4" id="dot-4"></div>
       </div>
     </div>
   );
